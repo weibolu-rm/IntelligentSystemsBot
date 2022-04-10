bec99540afd76854173ef8a70e24cec23e606e4a
Robert Michaud <robert.cn.michaud@hotmail.com>
2022-04-10 19:26:19 -0400

    spotlight

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 from intelligent_system_bot.rdf import DataBuilder
 from intelligent_system_bot.parse import TikaPreprocessor, SpacyNlp
+from intelligent_system_bot.spotlight import Spotlight
 
 def main():
     data_builder = DataBuilder(is_init=False)
     preprocessor = TikaPreprocessor()
     spacy_nlp = SpacyNlp()
+    spotlight = Spotlight()
 
     data_builder.run()
     preprocessor.run()
     spacy_nlp.run()
+
+    # TODO: pass spacy_nlp.tokens
+    spotlight.run({"Obama", "concordia university"})
+
+    
+
+
 
 if __name__ == "__main__":
     main()
